817a507cd51535ff7ffcb8c8471af14586d6cfaa
Samy Dindane <samy@dindane.com>
2012-01-16 11:32:02 +0100

    Màj du fichier bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,2 +1,25 @@
 <?php
 
+// initialiser Doctrine
+use Doctrine\ORM\Tools\Setup;
+require_once 'Doctrine/ORM/Tools/Setup.php';
+Setup::registerAutoloadPEAR();
+
+// configuration des annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(
+    array(__DIR__ . '/../entities', $isDevMode)
+);
+
+// configuration de la connexion
+$connection = array(
+    'driver'   => 'pdo_mysql',
+    'path'     => '127.0.0.1',
+    'dbname'   => 'cours_doctrine',
+    'user'     => 'cours_doctrine',
+    'password' => 'cours_doctrine_pwd',
+);
+
+$em = \Doctrine\ORM\EntityManager::create($connection, $config);
+
+session_start();
