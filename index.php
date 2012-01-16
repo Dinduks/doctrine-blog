e476c3c477a8e2e8f15116221f1650ecc0cc036e
Samy Dindane <samy@dindane.com>
2012-01-16 16:21:58 +0100

    Ajout de l'affichage des posts dans la page d'accueil

--- index.php
+++ index.php
@@ -9,11 +9,17 @@ $pageTitle = "Page d'accueil";
 // afficher le header
 require_once(__DIR__ . '/templates/header.php');
 
-/*
+$posts = $em->getRepository('Post')->findAll();
+?>
 
-Le code sera écrit ici
+<article style="margin-bottom:4em;">
+    <?php foreach ($posts as $i=>$post) : ?>
+    <h1><?php echo $post->getTitle() ?></h1>
+    <p><?php echo nl2br($post->getBody()) ?></p>
+    <hr />
+    <?php endforeach; ?>
+</article>
 
-*/
-
+<?php
 // afficher le footer
 require_once(__DIR__ . '/templates/footer.php');
